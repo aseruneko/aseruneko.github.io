a2984ce486fd73e6f8f7a4becf583215bc0ba996
あせるねこ <61032482+aseruneko@users.noreply.github.com>
2022-01-15 15:07:44 +0900

    Add files via upload

--- JARDLE/main.js
+++ JARDLE/main.js
@@ -1,23 +1,48 @@
 console.log("here");
 
-var correctAnswer = "しちごさん";
-var submittedAnswer = [];
+var correctAnswer = "きりたんぽ";
+var submittedCharacters = [];
+var gojuon = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをんがぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽゃゅょぁぃぅぇぉー"
+makeGojuon();
 
 function submit() {
     var input = document.getElementById("text-box").value;
-    submittedAnswer.push(input);
     renderAnswer(input);
 }
 
 function renderAnswer(input) {
     var textArea = document.getElementById("display-area");
     textArea.appendChild(makeAnswerDisplayNodes(input));
+    makeGojuon();
 }
+
+function makeGojuon() {
+    var gojuonArea = document.getElementById("gojuon-area");
+    var gojuonClone = gojuonArea.cloneNode( false );
+    gojuonArea.parentNode.replaceChild( gojuonClone , gojuonArea );
+    var gojuonArea = document.getElementById("gojuon-area");
+    var gojuons = gojuon.split("");
+    for(var i = 0; i < gojuons.length; i ++) {
+        var div = document.createElement("span");
+        if(submittedCharacters.includes(gojuons[i])) {
+            div.classList.add("used");
+        } else {
+            div.classList.add("not-used");
+        }
+        div.innerText = gojuons[i];
+        gojuonArea.appendChild(div);
+        if(i % 15 == 14) {
+            var br =document.createElement("br");
+            gojuonArea.appendChild(br);
+        }
+    }
+};
 
 function makeAnswerDisplayNodes(input) {
     var p = document.createElement("p");
     var inputs = input.split("");
     for(var i = 0; i < inputs.length; i ++) {
+        submittedCharacters.push(inputs[i]);
         if (isHit(inputs[i], i)) {
             var div = document.createElement("span");
             div.classList.add("green");
